21e67b125fdb374da0facd8c6ea0d15e9f4b3ae3
Diaz Lighuen <lighuen.diaz@gmail.com>
2024-08-26 21:06:23 +0100

    adding pagination

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,6 +3,11 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
-@Module({ imports: [TypeOrmModule.forFeature([Coffee])], controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({ 
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
+    controllers: [CoffeesController], 
+    providers: [CoffeesService]
+})
 export class CoffeesModule { }
